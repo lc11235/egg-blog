34c6a4773d45c5d7d8892c0efdccf6a00be9c23c
changli <lc11235@users.noreply.github.com>
2018-07-02 16:39:46 +0800

    add router change

--- app/router.js
+++ app/router.js
@@ -1,5 +1,10 @@
 
 module.exports = app => {
+  const { router, controller } = app;
+  // API
+  // ================ 后台 ================
+  // ------- 文章 -------
+  
   app.get('/api/article/list', app.controller.app.list);
   app.get('/api/article/:id', app.controller.app.detail);
   app.get('/*', app.controller.app.index);
